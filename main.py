288efce21adedda85af66b0c1f1bdaa199e8de7b
radekzien <radoslaw.zien@outlook.com>
2025-08-15 14:38:42 +0100

    Moved weather and sendEmail to subdirectories; Created a main class that handles Email sending to all subscribers

---
+++ main.py
@@ -0,0 +1,38 @@
+from db.db_fetch import fetch_subscribers, fetch_locations
+from weather.weather import get_weather
+from emailer.sendEmail import sendEmail
+
+def main():
+#-- Collecting weather data for all locations in the db --
+    all_locations = {}
+    # Fetch locations from the database
+    locations = fetch_locations()
+    
+    if not locations:
+        print("No locations found in the database.")
+        return
+    else:
+        print(f"Locations fetched: {locations}")
+
+    # Fetch weather for each location
+    for city in locations:
+        weather_data = get_weather(city)
+        all_locations[city] = weather_data
+
+#-- Sending weather data to subscribers --
+    subscribers = fetch_subscribers()
+    if not subscribers:
+        print("No subscribers found in the database.")
+        return
+    else:
+        print(f"Subscribers fetched: {subscribers}")
+
+    for subscriber in subscribers:
+        subscriberCity = subscriber[3]
+        subscriberEmail = subscriber[1]
+        if subscriberCity in all_locations:
+            content = all_locations[subscriberCity]
+            sendEmail(content, subscriberEmail)
+
+if __name__ == "__main__":
+    main()
